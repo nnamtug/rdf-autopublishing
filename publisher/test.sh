8809118ace089a04bd4db7d8bf6b249e746591e8
Christoph Gutmann <christoph.gutmann@gmx.ch>
2024-05-23 17:57:22 +0200

    add call curl template

--- publisher/test.sh
+++ publisher/test.sh
@@ -7,4 +7,11 @@ trap 'handle_error $LINENO' ERR
 
 
 echo gitRepo=${config[gitrepo]}
+
+
+### curl calling template
+#tempfile=$(mktemp)
+#resp=$(curl -s -w "%{http_code}" -o $tempfile -X POST -H 'Content-Type: application/rdf+xml' -H 'Accept: application/json' -G --data-urlencode "update=delete {?s ?p ?o} where {?s ?p ?o}" ${BASE}/statements)
+#checkCurlCall 204 $resp $tempfile
+
 echo "bye!"
