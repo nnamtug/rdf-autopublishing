8d1c65f142b1433d2af09ef6a562c057f851f965
Christoph Gutmann <christoph.gutmann@gmx.ch>
2024-05-23 16:19:22 +0200

    fix

--- docker/graphdb/run.sh
+++ docker/graphdb/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 sudo docker run \
-  --name graphdb-autopublisher-example \
-  --network autopublisher-example \
+  --name graphdb-auto-publisher \
+  --network auto-publisher \
+  -d \
+  --restart=unless-stopped \
+  -p 7200:7200 \
   ontotext/graphdb:9.10.2-se
